38214daaa5fb07eea12ee6776fbe1c464e7d08d1
AchkaN0n1x <sarah.svalero@gmail.com>
2016-11-16 17:58:05 -0500

    MAJ du fichier

--- test/emprunter_test.rb
+++ test/emprunter_test.rb
@@ -1,19 +1,21 @@
 require 'test_helper'
 require 'biblio'
 
-describe Biblio do
-  describe ".emprunter" do
-    let(:a) { Emprunt.new(toto, toto@blob.com, 'Harry Potter', 'JK Rowling') }
-    let(:b) { Emprunt.new(bob, bob@gmail.com, 'The Martian', 'Andy Weir') }
 
-    before @emprunts = [a,b]
+describe Biblio::Biblio do
+  describe ".emprunter" do
+    let(:a) { Biblio::Emprunt.new('toto', 'toto@blob.com', 'Harry Potter', 'JK Rowling')}
+    let(:b) { Biblio::Emprunt.new('bob','bob@gmail.com','The Martian','Andy Weir')}
+
+    before do @emprunts = [:a,:b] end
  
     describe "cas avec une demande d emprunt d un livre disponible" do
       it "retourne la table avec l ajout du nouvel emprunt" do
-        @new_emprunts = [a,b,Emprunt.new(pif, pif@gmail.com, 'Les Miserables', 'Victor Hugo')]
-        Biblio.emprunter(pif, pif@gmail.com, 'Les Miserables', 'Victor Hugo')
+        @new_emprunts = [a,b,Biblio::Emprunt.new('pif', 'pif@gmail.com', 'Les Miserables', 'Victor Hugo')]
+        emprunter('pif','pif@gmail.com','Les Miserables','Victor Hugo')
           .must_equal @new_emprunts
       end
     end
   end
 end
+
